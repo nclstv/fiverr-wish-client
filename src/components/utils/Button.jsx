8f8ff9ec08782c0d73ce631371c502e1c4f4dc50
Nicolas Stevens <stvns.ncls@gmail.com>
2023-09-30 16:42:11 +0200

    gradient

--- src/components/utils/Button.jsx
+++ src/components/utils/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button({ children }) {
   return (
-    <button className="w-full bg-green-500 text-white text-lg font-medium py-3 rounded-md round ">
+    <button className="w-full bg-gradient-to-r from-green-500 to-lime-500 text-white text-lg font-medium py-3 rounded-md round ">
       {children}
     </button>
   );
